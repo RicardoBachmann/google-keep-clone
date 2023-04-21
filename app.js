c384bc782c8308372fe17865e46d0e0c8b5065f0
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-04-21 09:26:31 +0200

    CloseForm() works

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 class App {
   constructor() {
-    this.$form = document.querySelector('#form');
-    this.$noteTitle = document.querySelector('#note-title');
-    this.$fromButtons = document.querySelector('#form-buttons');
+    this.$form = document.querySelector("#form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$fromButtons = document.querySelector("#form-buttons");
 
     this.addEventListener();
   }
 
   addEventListener() {
-    document.body.addEventListener('click', event => {
+    document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
     });
-
   }
 
   handleFormClick(event) {
-    const isFormClicked = this.$form.contains(event.target); 
-    
+    const isFormClicked = this.$form.contains(event.target);
+
     if (isFormClicked) {
       this.openForm();
     } else {
-        // close form
-    };
-  }
-    openForm() {
-        this.$form.classList.add('form-open');
-        this.$noteTitle.style.display = 'block';
-        this.$formButtons.style.display = 'block';
+      this.closeForm();
     }
   }
+  openForm() {
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
+
+  closeForm() {
+    this.$form.classList.remove("form-open");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
+  }
+}
 
 new App();
